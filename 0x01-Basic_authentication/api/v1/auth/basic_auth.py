a1aa31b779e207f9f007ab1d24c4bab001275b3f
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-03-07 00:14:46 +0100

    extracts the validation credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@
 """
 Implememt basic authentication
 """
-
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -10,4 +10,15 @@ class BasicAuth(Auth):
     """
     Basic authentication
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        returns the Base64 part of the
+        Authorization header for a Basic Authentication
+        """
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+        if authorization_header[:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
