dcb4907db2c7b0359e6b16cf504ae6e0350b16b9
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-03-08 21:23:54 +0100

    added a method that implements an “in-memory” Session ID storing. This will enable retrieval of a User based on session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,22 @@
 session authentication
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     This class implements the session authentication for the API
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            user_id_by_session_id[session_id] = user_id
+            return session_id
