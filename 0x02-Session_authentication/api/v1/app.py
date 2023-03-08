058b6165546bcfe4c61a5a2e9661f6d8576dc600
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-03-09 00:51:46 +0100

    update Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,9 +43,8 @@ def before_req_func():
         return None
     if auth.require_auth(request.path, excluded_paths) is False:
         return None
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None and\
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
