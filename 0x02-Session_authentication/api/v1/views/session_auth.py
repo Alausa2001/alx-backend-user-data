b5c96a806cd411e9b0bde4af7b9e7b885c220385
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-03-09 03:16:29 +0100

    Created a new Flask view that handles all routes for the Session authentication

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+view that handles all routes for the Session authentication.
+"""
+
+import os
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
+def login():
+    """
+    implements user login
+    """
+    password = request.form.get('password', None)
+    email = request.form.get('email', None)
+
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    if password is None:
+        return jsonify({"error": "password missing"}), 400
+
+    # User.load_from_file()
+    user_exist = User.search({'email': email})
+    if not user_exist:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in user_exist:
+        if user.is_valid_password(password) is False:
+            return jsonify({"error": "wrong password"}), 401
+        try:
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+            return response
+        except Exception:
+            return jsonify({"error": "no user found for this email"}), 404
