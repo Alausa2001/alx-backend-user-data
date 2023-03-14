3046456f0a74edd6f280774811e9877687e037bf
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-03-14 23:26:45 +0100

    added a add_user method to add a new user to the database (no authentication is done here)

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,12 +30,12 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         adds a user to the database
         """
         # lf.__session = self._session
-        new_user = User(email=email, hashed_password=password)
+        new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
