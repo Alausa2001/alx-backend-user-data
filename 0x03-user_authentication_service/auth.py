31636d7a2c419b11bf355131cae79dc99a405f80
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-03-15 15:06:28 +0100

    added a reset password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -114,7 +114,6 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
-
     def update_password(self, reset_token: str, password: str) -> None:
         """
         updates password
@@ -122,14 +121,7 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_pwd = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed_pwd)
+            self._db.update_user(user.id, hashed_password=hashed_pwd,
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
-
-
-
-
-
-
-
-
