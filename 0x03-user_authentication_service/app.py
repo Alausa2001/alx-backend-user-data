4cdc5a1d2c65c22515924c867e66e7a808678938
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-03-15 14:17:17 +0100

    added a route to get a user via the session id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,5 +70,18 @@ def logout():
     abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """
+    checks if a session_id is still on for a user
+    """
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": f"{user.email}"})
+    abort(403)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
